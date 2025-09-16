d32fe39c2ca76fb35503140c81e383afb6371075
João Pedro Coriliano <joaopedro.coriliano@gmail.com>
2025-09-16 19:13:30 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Cananéia");
-Console.WriteLine("Iguape");
-Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Santos");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Cubatão");
 Console.WriteLine("Itanhaém");
 Console.WriteLine("Mongaguá");
 Console.WriteLine("Peruíbe");
